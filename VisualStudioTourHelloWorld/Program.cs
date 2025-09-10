7ac21c2d2404b32d63b7ed2ace07e064ad01eb48
Opilane TTHK <opilane@tthk.ee>
2025-09-10 10:34:38 +0300

    Eemalda vana kood

--- VisualStudioTourHelloWorld/Program.cs
+++ VisualStudioTourHelloWorld/Program.cs
@@ -2,63 +2,8 @@
 {
     internal class Program
     {
-        static string AReallyCoolFunction()
-        {
-            /* 
-             väga sekreetne funktsioon.
-            töötab hästi minu arvutis aga kui sul ei tööta siis unluck
-             */
-            string VeryImportantPassword = "qwerty"; // väga oluline NDA stuff. donˇt share
-            return VeryImportantPassword;
-        }
-
         static void Main(string[] args)
         {
-            /* int foo = 5;
-            int bar = 6;
-
-            Console.WriteLine(foo + bar);
-
-            Console.WriteLine("Hello, World!");
-            
-
-            string SomethingVeryImportant = AReallyCoolFunction();
-            Console.WriteLine(SomethingVeryImportant);
-
-            int VeryImportantNumber = 5;
-            float AnotherVeryImportantNumber = 5.2f;
-            double YetAnotherVeryImportantNumber = 1.4;
-
-            float esimene_tulemus = VeryImportantNumber + AnotherVeryImportantNumber;
-            double tulemus = VeryImportantNumber + AnotherVeryImportantNumber + YetAnotherVeryImportantNumber;
-
-            Console.WriteLine(esimene_tulemus);
-            Console.WriteLine(tulemus);
-
-
-            Console.WriteLine("Palun sisetage teie nimi: ");
-            string yourname = Console.ReadLine();
-            Console.WriteLine("Tere, " + yourname);
-            */
-
-
-            /*
-            Console.WriteLine("sisestage numbri palun aitah");
-            int teine_depends = int.Parse(Console.ReadLine());
-            switch (teine_depends)
-            {
-                default:
-                    Console.WriteLine("default case");
-                    return;
-                case 1:
-                    Console.WriteLine("case 1");
-                    return;
-                case 69:
-                    Console.WriteLine("nice");
-                    return;
-            }*/
-
-
             Console.WriteLine("Tere. Sisestage esimene number pls");
             int arv1 = int.Parse(Console.ReadLine());
 
